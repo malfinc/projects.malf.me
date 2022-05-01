77003295ec181ebf6a72e7ad09b49f55ceeb0f74
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2022-05-01 00:10:18 -0700

    More improvements

--- lib/core_web/controllers/page_controller.ex
+++ lib/core_web/controllers/page_controller.ex
@@ -4,4 +4,24 @@ defmodule CoreWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def show(conn, %{"id" => "socials"}) do
+    render(conn, "socials.html")
+  end
+
+  def show(conn, %{"id" => "discord"}) do
+    render(conn, "discord.html")
+  end
+
+  def show(conn, %{"id" => "about"}) do
+    render(conn, "about.html")
+  end
+
+  def show(conn, %{"id" => "projects"}) do
+    render(conn, "projects.html")
+  end
+
+  def show(conn, %{"id" => "contact"}) do
+    render(conn, "contact.html")
+  end
 end
