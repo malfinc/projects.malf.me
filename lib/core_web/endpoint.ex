4de8c25b5ed87ce3fe60782766d9d492d29af803
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2022-11-12 23:58:20 -0800

    Removing unneeded code

--- lib/core_web/endpoint.ex
+++ lib/core_web/endpoint.ex
@@ -5,11 +5,6 @@ defmodule CoreWeb.Endpoint do
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
-  # @session_options [
-  #   store: :cookie,
-  #   key: "_core_key",
-  #   signing_salt: "JKEx/AEF"
-  # ]
   @session_options [
     store: PhoenixLiveSession,
     pub_sub: Core.PubSub,
