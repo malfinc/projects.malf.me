a41f9741435731bf97d1879165703e879cbd4736
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2023-01-04 15:12:52 -0800

    Fixing migration

--- priv/repo/migrations/20230104221314_add_reason_to_coin_transactions.exs
+++ priv/repo/migrations/20230104221314_add_reason_to_coin_transactions.exs
@@ -2,5 +2,8 @@ defmodule Core.Repo.Migrations.AddReasonToCoinTransactions do
   use Ecto.Migration
 
   def change do
+    alter(table(:coin_transactions)) do
+      add :reason, :text, null: false, default: "unknown"
+    end
   end
 end
